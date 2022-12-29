dd403be785c504fdc4b990f494d1f6ae3c57f3ce
siddharth_chatterjee <siddharthchatterjee99@gmail.com>
2022-12-29 21:57:52 +0530

    Commit Hello ISS Governance

---
+++ Java/src/corejava/HelloISS.java
@@ -0,0 +1,8 @@
+package corejava;
+
+public class HelloISS {
+    public static void main (String[] args) {
+        System.out.println("Hello ISS Governance");
+        System.out.println("from Siddharth Chatterjee");
+    }
+}
